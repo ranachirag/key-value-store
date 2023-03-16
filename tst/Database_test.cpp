2cdd89aaa7f2056ca11d1473b7505c15977889c2
Jhanvi <33077901+rutavshah@users.noreply.github.com>
2023-03-16 15:08:52 -0400

    Added DB tests

--- tst/Database_test.cpp
+++ tst/Database_test.cpp
@@ -17,24 +17,54 @@ class Database_test : public ::testing::Test{
 
     Database_test(){};
 
-    virtual void SetUp() override {
-      db = new Database(100);
-      db->open("testdb");
-      std::cout << "SetUp called";
-    }
+    // virtual void SetUp() override {
+    //   db = new Database(100);
+    //   db->open("testdb");
+    //   std::cout << "SetUp called";
+    // }
 
-    virtual void TearDown() override {
-      std::cout << "TearDown called";
-      db->close();
-      delete db;
-    }
+    // virtual void TearDown() override {
+    //   std::cout << "TearDown called";
+    //   db->close();
+    //   delete db;
+    // }
 };
 
-TEST_F(Database_test, getOpentest) {
+TEST_F(Database_test, opentest) {
+  db = new Database(100);
+  db->open("testdb");
   const char* expected_error_message = "Please close the testdb database first";
   testing::internal::CaptureStderr();
   db->open("testdb");
   std::string err_msg = testing::internal::GetCapturedStderr();
   std::string actual_error_message = err_msg.substr(0, err_msg.find(":"));
   EXPECT_EQ(actual_error_message, expected_error_message);
+  db->close();
+  delete db;
+}
+
+TEST_F(Database_test, putGetTest) {
+  db = new Database(100);
+  db->open("testputget");
+  db->put(0,0);
+  ASSERT_EQ(db->get(0), 0);
+  db->close();
+  delete db;
+}
+
+TEST_F(Database_test, scanTest) {
+  db = new Database(100);
+  db->open("testscan");
+  ASSERT_EQ(db->get(0, -1);
+  db->put(0,0);
+  db->put(1,1);
+  db->put(2,2);
+  db->put(3,3);
+  db->put(5,5);
+  std::vector<std::pair<long, long> > lst;
+  ASSERT_EQ(db->scan(lst, 1, 1), 1);
+  ASSERT_EQ(db->scan(lst, 0, 4), 4);
+  ASSERT_EQ(db->scan(lst, 0, 5), 5);    
+  db->close();
+  delete db;
 }
